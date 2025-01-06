d0dbba0cbcaacf128c2a5ccedac42c84834dda31
rockstardev <5191402+rockstardev@users.noreply.github.com>
2025-01-05 18:41:07 -0600

    Implementing Payment Methods endpoint

---
+++ src/Strike.Client/PaymentMethods/PaymentAccountTypes.cs
@@ -0,0 +1,25 @@
+﻿namespace Strike.Client.PaymentMethods;
+
+/// <summary>
+/// Payment method account types
+/// </summary>
+public enum PaymentMethodAccountTypes
+{
+	/// <summary>
+	/// CHECKING
+	/// </summary>
+	[EnumMember(Value = "CHECKING")]
+	Checking,
+
+	/// <summary>
+	/// SAVINGS
+	/// </summary>
+	[EnumMember(Value = "SAVINGS")]
+	Savings,
+
+	/// <summary>
+	/// <para>Catch-all for unknown values returned by Strike. If you encounter this, please check if there is a later version of the Strike.Client library.</para>
+	/// </summary>
+	[EnumMember(Value = "UNDEFINED")]
+	Undefined
+}
---
+++ src/Strike.Client/PaymentMethods/PaymentMethodState.cs
@@ -0,0 +1,43 @@
+﻿namespace Strike.Client.PaymentMethods;
+
+/// <summary>
+/// Payment method state
+/// </summary>
+public enum PaymentMethodState
+{
+	/// <summary>
+	/// The payment method is pending
+	/// </summary>
+	[EnumMember(Value = "PENDING")]
+	Pending,
+
+	/// <summary>
+	/// The payment method is ready to be used
+	/// </summary>
+	[EnumMember(Value = "READY")]
+	Ready,
+
+	/// <summary>
+	/// The payment method is suspended
+	/// </summary>
+	[EnumMember(Value = "SUSPENDED")]
+	Suspended,
+
+	/// <summary>
+	/// The payment method is invalid
+	/// </summary>
+	[EnumMember(Value = "INVALID")]
+	Invalid,
+
+	/// <summary>
+	/// The payment method is inactive
+	/// </summary>
+	[EnumMember(Value = "INACTIVE")]
+	Inactive,
+
+	/// <summary>
+	/// <para>Catch-all for unknown values returned by Strike. If you encounter this, please check if there is a later version of the Strike.Client library.</para>
+	/// </summary>
+	[EnumMember(Value = "UNDEFINED")]
+	Undefined
+}
